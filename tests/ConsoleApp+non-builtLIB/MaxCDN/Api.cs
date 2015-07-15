65811a75baea0a0154862ee9fa56d2c3bdf09193
Ivan Dabic <idabic@netdna.com>
2015-07-15 06:34:13 -0700

    Updated to 2.0

--- tests/ConsoleApp+non-builtLIB/MaxCDN/Api.cs
+++ tests/ConsoleApp+non-builtLIB/MaxCDN/Api.cs
@@ -1,9 +1,7 @@
-﻿using System;
+using System;
 using System.ComponentModel;
 using System.Net;
 using System.Web;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.Text;
 
 namespace MaxCDN
@@ -13,11 +11,11 @@ namespace MaxCDN
         private string _consumerKey = "";
         private string _consumerSecret = "";
         private string _alias = "";
-        private int _requestTimeout = 30;
+        private int _requestTimeout = 180;
 
         private const string _MaxCDNBaseAddress = "https://rws.maxcdn.com";
 
-        public Api(string alias, string consumerKey, string consumerSecret, int requestTimeout = 30)
+        public Api(string alias, string consumerKey, string consumerSecret, int requestTimeout = 180)
         {
             _consumerKey = consumerKey;
             _alias = alias;
@@ -26,28 +24,71 @@ namespace MaxCDN
         }
 
         public dynamic Get(string url, bool debug = false)
-        {            
-            var requestUrl = GenerateOAuthRequestUrl(url, "GET");
-
-            var request = new ApiWebClient(_requestTimeout);            
-            var response = request.DownloadString(requestUrl);
-
-            var result = JObject.Parse(response);
+        {
             if (debug)
-                DumpObject(result);
-
-            return result;
+            {
+                Console.Write("Running garbage collector\n");
+            }
+            clear(debug);
+            if (debug)
+            {
+                Console.Write("Done\n\nGenerating OAuth request URL for " + url + "\n");
+            }
+            var requestUrl = GenerateOAuthRequestUrl(url, "GET");
+            if (debug)
+            {
+                Console.Write("Done: \n" + requestUrl + "\n\nCreating request\n");
+            }
+            var request = new ApiWebClient(_requestTimeout);
+            if (debug)
+            {
+                Console.Write("Done: \n" + request + "\n Defining User Agent for API request\n");
+            }
+            request.Headers.Add("user-agent", "MaxCDN dot-net API Client");
+            if (debug)
+            {
+                Console.Write("Done:\nUser Agent = MaxCDN dot-net API Client\n\nMaking the GET request\n");
+            }
+            var response = request.DownloadString(requestUrl);
+            if (debug)
+            {
+                DumpObject(response);
+            }
+            return response;
         }
 
         //DELETE requests handler
-        public bool Delete(string url)
+        public bool Delete(string url, bool debug = false)
         {
+            if (debug)
+            {
+                Console.Write("Running garbage collector\n");
+            }
+            clear(debug);
+            if (debug)
+            {
+                Console.Write("Done\n\n");
+            }
             var response = GetWebResponse(url, "DELETE");
+            if (debug)
+            {
+                Console.Write("Done:\n" + response + "\n\n");
+                DumpObject(response);
+            }
             return ((HttpWebResponse)response).StatusCode == HttpStatusCode.OK;
         }
         //PURGE-DELETE requests handler
-        public bool Purge(string url, string uri)
-        {            
+        public bool Purge(string url, string uri, bool debug = false)
+        {
+            if (debug)
+            {
+                Console.Write("Running garbage collector\n");
+            }
+            clear(debug);
+            if (debug)
+            {
+                Console.Write("Done\n\nEncoding request URi\n");
+            }
             char[] enc = System.Web.HttpUtility.UrlEncode(uri).ToCharArray();
 
             for (int i = 0; i < enc.Length - 2; i++)
@@ -60,9 +101,141 @@ namespace MaxCDN
             }
 
             string encfinal = new string(enc);
-            
-                var response = GetWebResponse(url + "?" + encfinal.Replace("%5D%3D", "%5D=").Replace("%26file%5B", "&file%5B"), "DELETE");
-            
+
+            if (debug)
+            {
+                Console.Write("Done:\n" + encfinal + "\n\nMaking the DELETE request\n");
+            }
+
+            var response = GetWebResponse(url + "?" + encfinal.Replace("file%3D", "file=").Replace("%5D%3D", "%5D=").Replace("%26file%5B", "&file%5B"), "DELETE");
+            if (debug)
+            {
+                Console.Write("Done:\n");
+                DumpObject(response);
+            }
+            return ((HttpWebResponse)response).StatusCode == HttpStatusCode.OK;
+        }
+
+        //PUT requests handler
+        public bool Put(string url, dynamic data, bool debug = false)
+        {
+            if (debug)
+            {
+                Console.Write("Running garbage collector\n");
+            }
+            clear(debug);
+            if (debug)
+            {
+                Console.Write("Done\n\nEncoding request URi\n");
+            }
+            char[] enc = System.Web.HttpUtility.UrlEncode(data).ToCharArray();
+
+            for (int i = 0; i < enc.Length - 2; i++)
+            {
+                if (enc[i] == '%')
+                {
+                    enc[i + 1] = char.ToUpper(enc[i + 1]);
+                    enc[i + 2] = char.ToUpper(enc[i + 2]);
+                }
+            }
+
+            string encfinal = new string(enc);
+            data = encfinal;
+            data = data.Replace("%3D", "=").Replace("%26", "&");
+
+            if (debug)
+            {
+                Console.Write("Done:\n" + encfinal + "\n\nMaking the PUT request\n Generating OAuth request for: " + url + "\n");
+            }
+
+            var requestUrl = GenerateOAuthRequestUrl(url, "PUT");
+            if (debug)
+            {
+                Console.Write(" Done:\n" + requestUrl + "\n  Creating API Web client request\n");
+            }
+            var request = WebRequest.Create(requestUrl);
+            if (debug)
+            {
+                Console.Write(" Done:\n" + request + "\n  Defining request method (PUT)\n");
+            }
+            request.Method = "PUT";
+            if (debug)
+            {
+                Console.Write(" Done\n  Reading response\n");
+            }
+            byte[] byteArray = Encoding.UTF8.GetBytes(data);
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+            var dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            var response = request.GetResponse();
+            if (debug)
+            {
+                DumpObject(response);
+            }
+            return ((HttpWebResponse)response).StatusCode == HttpStatusCode.OK;
+        }
+        //POST request handler
+        public bool Post(string url, dynamic data, bool debug = false)
+        {
+            if (debug)
+            {
+                Console.Write("Running garbage collector\n");
+            }
+            clear(debug);
+            if (debug)
+            {
+                Console.Write("Done\n\nEncoding request URi\n");
+            }
+            char[] enc = System.Web.HttpUtility.UrlEncode(data).ToCharArray();
+
+            for (int i = 0; i < enc.Length - 2; i++)
+            {
+                if (enc[i] == '%')
+                {
+                    enc[i + 1] = char.ToUpper(enc[i + 1]);
+                    enc[i + 2] = char.ToUpper(enc[i + 2]);
+                }
+            }
+
+            string encfinal = new string(enc);
+            data = encfinal;
+            data = data.Replace("%3D", "=").Replace("%26", "&");
+
+            if (debug)
+            {
+                Console.Write("Done:\n" + data + "\nGenerating OAuth request URi\n\n");
+            }
+
+            var requestUrl = GenerateOAuthRequestUrl(url, "POST");
+            if (debug)
+            {
+                Console.Write("Done\n\nMaking the POST request with " + requestUrl + "\n\n");
+            }
+            var request = WebRequest.Create(requestUrl);
+            if (debug)
+            {
+                Console.Write("Done:\n" + request + "\n\nDefining request method (POST)\n");
+            }
+            request.Method = "POST";
+            if (debug)
+            {
+                Console.Write("Done\n\nReading response\n");
+            }
+            byte[] byteArray = Encoding.UTF8.GetBytes(data);
+            request.ContentType = "application/json";
+            request.ContentLength = byteArray.Length;
+
+            var dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            var response = request.GetResponse();
+            if (debug)
+            {
+                DumpObject(response);
+            }
             return ((HttpWebResponse)response).StatusCode == HttpStatusCode.OK;
         }
 
@@ -70,81 +243,13 @@ namespace MaxCDN
         private WebResponse GetWebResponse(string url, string method)
         {
             var requestUrl = GenerateOAuthRequestUrl(url, method);
-
-            var request = WebRequest.Create(requestUrl);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(requestUrl);
             request.Method = method;
-
+            request.UserAgent = "MaxCDN dot-net API Client";
             var response = request.GetResponse();
             return response;
         }
 
-        //PUT requests handler
-        public bool Put(string url, dynamic data)
-        {
-            char[] enc = System.Web.HttpUtility.UrlEncode(data).ToCharArray();
-
-            for (int i = 0; i < enc.Length - 2; i++)
-            {
-                if (enc[i] == '%')
-                {
-                    enc[i + 1] = char.ToUpper(enc[i + 1]);
-                    enc[i + 2] = char.ToUpper(enc[i + 2]);
-                }
-            }
-
-            string encfinal = new string(enc);
-            data = encfinal;
-            data = data.Replace("%3D", "=").Replace("%26", "&");
-
-            var requestUrl = GenerateOAuthRequestUrl(url, "PUT");
-            var request = WebRequest.Create(requestUrl);
-            request.Method = "PUT";
-            byte[] byteArray = Encoding.UTF8.GetBytes(data);
-            request.ContentType = "application/json";
-            request.ContentLength = byteArray.Length;
-            var dataStream = request.GetRequestStream();
-            dataStream.Write(byteArray, 0, byteArray.Length);
-            dataStream.Close();
-            var response = request.GetResponse();
-            return ((HttpWebResponse)response).StatusCode == HttpStatusCode.OK;
-        }
-        //POST request handler
-        public bool Post(string url, dynamic data)
-        {
-            char[] enc = System.Web.HttpUtility.UrlEncode(data).ToCharArray();
-
-            for (int i = 0; i < enc.Length - 2; i++)
-            {
-                if (enc[i] == '%')
-                {
-                    enc[i + 1] = char.ToUpper(enc[i + 1]);
-                    enc[i + 2] = char.ToUpper(enc[i + 2]);
-                }
-            }
-
-            string encfinal = new string(enc);
-            data = encfinal;
-            data = data.Replace("%3D", "=").Replace("%26", "&");
-
-
-            var requestUrl = GenerateOAuthRequestUrl(url, "POST");
-
-            var request = WebRequest.Create(requestUrl);
-            request.Method = "POST";
-
-            byte[] byteArray = Encoding.UTF8.GetBytes(data);
-            request.ContentType = "application/json";
-            request.ContentLength = byteArray.Length;
-
-            var dataStream = request.GetRequestStream();
-            dataStream.Write(byteArray, 0, byteArray.Length);
-            dataStream.Close();
-
-            var response = request.GetResponse();
-
-            return ((HttpWebResponse)response).StatusCode == HttpStatusCode.OK;
-        }
-        
         //Generate request url
 
         private string GenerateOAuthRequestUrl(string url, string method)
@@ -181,9 +286,9 @@ namespace MaxCDN
         //Initialize WebClient
         private class ApiWebClient : WebClient
         {
-            private int _requestTimeout = 30;
+            private int _requestTimeout = 180;
 
-            public ApiWebClient(int requestTimeout = 30)
+            public ApiWebClient(int requestTimeout = 180)
             {
                 _requestTimeout = requestTimeout;
             }
@@ -194,6 +299,15 @@ namespace MaxCDN
                 w.Timeout = _requestTimeout * 1000;
                 return w;
             }
+        }
+        public void clear(bool debug)
+        {
+            if (debug)
+            {
+                Console.WriteLine("Total Memory Used: {0}\n", GC.GetTotalMemory(true));
+                Console.WriteLine("Garbage Count for current generation collection: {0}\n", GC.CollectionCount(GC.GetGeneration(0)));
+            }
+            GC.Collect();
         }
     }
 }
